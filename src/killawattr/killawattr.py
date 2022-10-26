f4f0b5052ec3480742ba6b06d5c48701ae492420
lex <lex@users.noreply.github.com>
2022-10-26 22:52:08 +0300

    add fetching the data

--- src/killawattr/killawattr.py
+++ src/killawattr/killawattr.py
@@ -1,3 +1,20 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+
 class Killawattr:
-    def __init__(self, api_url):
+    def __init__(self, api_url, username, password):
         self.api_url = api_url
+        self.username = username
+        self.password = password
+
+    def fetch_data(self, filename):
+        auth = HTTPBasicAuth(self.username, self.password)
+        headers = {'Content-type': 'application/json'}
+        response = requests.get(
+            f'{self.api_url}/{filename}', auth=auth, headers=headers)
+
+        if response.status_code != 200:
+            return None
+
+        return response.json()
--- tests/test_killawattr.py
+++ tests/test_killawattr.py
@@ -1,10 +1,40 @@
 from killawattr.killawattr import Killawattr
 
 API_URL = 'https://api.com'
+USERNAME = 'username'
+PASSWORD = 'password'
 
 
-class TestKillawattr:
-    k = Killawattr(API_URL)
+class TestKillawattrFetching:
+    k = Killawattr(API_URL, USERNAME, PASSWORD)
+    method = 'GET'
+    filename = 'asd'
+    json = {'ok': 'ok'}
+    url = f'{API_URL}/{filename}'
 
     def test_given_api_url_is_saved(self):
         assert self.k.api_url == API_URL
+
+    def test_fetching_uses_given_api_url(self, requests_mock):
+        requests_mock.register_uri(
+            self.method, self.url, json=self.json, status_code=200)
+        data_json = self.k.fetch_data(self.filename)
+        assert requests_mock.called
+
+    def test_fetching_uses_http_basic_auth(self, requests_mock):
+        requests_mock.register_uri(
+            self.method, self.url, json=self.json, status_code=200)
+        data_json = self.k.fetch_data(self.filename)
+        assert 'Basic' in requests_mock.last_request.headers['Authorization']
+
+    def test_fetching_returns_the_json_if_http_200(self, requests_mock):
+        requests_mock.register_uri(
+            self.method, self.url, json=self.json, status_code=200)
+        data_json = self.k.fetch_data(self.filename)
+        assert data_json == self.json
+
+    def test_fetching_returns_none_if_not_http_200(self, requests_mock):
+        requests_mock.register_uri(
+            self.method, self.url, json=self.json, status_code=404)
+        data_json = self.k.fetch_data(self.filename)
+        assert data_json == None
